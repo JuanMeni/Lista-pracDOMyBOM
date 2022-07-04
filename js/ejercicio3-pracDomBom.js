c63980cc9f2fede433ff54efae91a30c806a9761
Juan Menichetti <jpmenichetti13@gmail.com>
2022-07-04 17:18:55 -0300

    se agrego la funcionalidad de los botones

--- js/ejercicio3-pracDomBom.js
+++ js/ejercicio3-pracDomBom.js
@@ -1,5 +1,20 @@
-/*Crea una web con bootstrap y js, que contenga un botón input donde se pueda cargar
- una tarea y un botón que al ser presionado agregue dicha tarea a una lista, 
- cada elemento ingresado en la lista debe poder ser eliminado con un botón creado 
- para ese fin. 
- */
+// /*Crea una web con bootstrap y js, que contenga un botón input donde se pueda cargar
+//  una tarea y un botón que al ser presionado agregue dicha tarea a una lista,
+//  cada elemento ingresado en la lista debe poder ser eliminado con un botón creado
+//  para ese fin.
+//  */
+let lista = document.getElementById('lista')
+let tarea = document.getElementById('tareaInput')
+
+function btnAgregar(){
+  let li = document.createElement("li")
+  li.setAttribute('id', tarea.value)
+  li.appendChild(document.createTextNode(tarea.value))
+  lista.appendChild(li)
+}
+
+function btnEliminar(){
+  let item = document.getElementById(tarea.value)
+  lista.removeChild(item)
+}
+
